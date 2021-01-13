a38ea09553af8f4586257a72881f48611931d7b3
tati oliveira <tat1roliv@gmail.com>
2021-01-13 18:18:50 -0300

    Create script.js

---
+++ Javascript/m2-basics/script.js
@@ -0,0 +1,13 @@
+//var
+//string
+var nome = "Boni";
+//number
+var idade = parseInt(90);
+console.log(idade);
+//float
+var preco = parseFloat(5.50);
+console.log(preco);
+//boolean(truexfalse)
+var salvo = true;
+//undefined
+var cidade;
