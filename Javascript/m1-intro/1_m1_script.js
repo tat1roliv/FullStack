cf4cae005e5d3830a70261b3e7c9cd7a873bbaf1
tati oliveira <tat1roliv@gmail.com>
2021-01-13 17:11:41 -0300

    create js

---
+++ Javascript/m1-intro/1_m1_script.js
@@ -0,0 +1,15 @@
+//EXIBIR INFORMACOES AO USUARIO/DEV
+
+//DOCUMENT
+//document.getElementById("id").innerHTML
+document.getElementById("titulo").innerHTML = "hola";
+//document.write("txt on DOM")
+document.write("holaaaaaaa");
+
+//WINDOW
+//alert
+window.alert("houulaa");
+
+//CONSOLE
+//console.log
+console.log("mensagem no console")
