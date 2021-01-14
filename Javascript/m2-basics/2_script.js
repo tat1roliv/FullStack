e4763422a7ff6762d5b70c4a2e47f28bbc3807cd
tati oliveira <tat1roliv@gmail.com>
2021-01-14 12:05:05 -0300

    create js 2

---
+++ Javascript/m2-basics/2_script.js
@@ -0,0 +1,10 @@
+//getElementbyId   
+document.getElementById("campo")
+//innerHTML
+document.getElementById("titulo").innerHTML = "hola"
+//var tag html (inner HTML -> conteudo da tag)
+var titulo = document.getElementById("titulo")
+titulo.innerHTML = "hola que tal"
+//input -> value  (value -> nao tem conteudo efetivo, so atributos)
+var campo = document.getElementById("campo")
+campo.value = "buenasss"
