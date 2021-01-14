60f4ac3772f768f3168ac56a6b865befc1f7aed8
tati oliveira <tat1roliv@gmail.com>
2021-01-14 13:01:07 -0300

    Update 3_script.js

--- Javascript/m2-basics/3_script.js
+++ Javascript/m2-basics/3_script.js
@@ -13,4 +13,10 @@ alterar(titulo){
         document.getElementById("titulo").innerHTML = titulo;
         document.getElementById("campo").value = titulo;
     }
+alterar("Esse é o titulo")
 */
+
+function somar(x,y){
+    let total = x + y;
+    document.getElementById("campo").value = total;
+}
