9b9650e4e2b732a21bb87cfb0a4fcdb0cb83c8ba
tati oliveira <tat1roliv@gmail.com>
2021-01-14 12:53:49 -0300

    update functions

---
+++ Javascript/m2-basics/3_script.js
@@ -0,0 +1,16 @@
+//functions
+//func1
+function alterar(){
+    document.getElementById("titulo").innerHTML = "function to change <h1>";
+    document.getElementById("campo").value = "function to change <input>";
+}
+alterar();
+
+/*
+function(variavel) 
+    
+alterar(titulo){
+        document.getElementById("titulo").innerHTML = titulo;
+        document.getElementById("campo").value = titulo;
+    }
+*/
