f74c5ef1d707f749a2e56fb1ccd51bd825371246
tati oliveira <tat1roliv@gmail.com>
2021-01-16 20:25:05 -0300

    update

--- reactlogin/src/App.js
+++ reactlogin/src/App.js
@@ -1,18 +1,20 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 
-
 const Input = styled.input`
-  display: down
+  border: 1px solid red;
+  width:300px;
+  height: 25px;
+  padding: 10px;
+
 `;
-  
-
-
+ 
 function App() {
 
+  const [ texto, setTexto ] = useState('tati1')
   return (
     <>
-      <Input type="text"/>
+      <Input type="text" value={texto}/>
     </>
   );
 }
