6277778863da207c5b8c550fd1700e945218555f
tati oliveira <tat1roliv@gmail.com>
2021-01-16 13:56:33 -0300

    xxx

--- React/reacttest/src/App.js
+++ React/reacttest/src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
 import './App.css';
 
-function Avatar(props) {
-  return (
-    <div className="avatar">
-      <img src={props.user.url} alt={props.user.name}/>
-      <br/>
-      <span>{props.user.name}</span>
-    </div>
-  );
-}
-
 function App(){
-  let user = {
-    url:"https://www.google.com.br/google.jpg",
-    name:"tat1"
-  };
-
-return <>
-    <Avatar user={user}/>
+  return <>
+    
   </>;
 }
+
 export default App;
