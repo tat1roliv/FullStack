5654a5b1c73e7e6dfca1eab479d38fd178f0134d
tati oliveira <tat1roliv@gmail.com>
2021-01-27 17:10:47 -0300

    Update menuNavScript.js

--- Javascript/m2-basics/menuNavScript.js
+++ Javascript/m2-basics/menuNavScript.js
@@ -15,3 +15,17 @@ function toggleMenu(){
     }
 
 }
+
+/*
+
+let menuArea = document.getElementById("menu-area");
+
+    if (menuArea.style.width == '200px') {
+        menuArea.style.width = '0px';
+
+    } else {
+        menuArea.style.width = '200px'
+    }
+
+
+*/ 
