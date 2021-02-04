9c340363cc15b8f41e979c2d85b9432438e8ac18
tati oliveira <tat1roliv@gmail.com>
2021-02-04 18:00:36 -0300

    Create numbersMethods.js

---
+++ Javascript/m3-manipulation/numbersMethods.js
@@ -0,0 +1,19 @@
+//numbers methods
+
+//toString
+let n = 10;
+let res = n.toString(); //tranforma em string + toda aquela loucura de concatenação string X num no Js
+console.log(res);
+
+//toFixed(parametro <n casas decimais>)
+let piii = 3.1415926535;
+piii.toFixed(2);
+console.log(piii);
+
+//parseInt
+let notNumber = '2555';
+parseInt(notNumber);
+
+//parseFloat
+let notReal = '1,99';
+parseFloat(notReal);
