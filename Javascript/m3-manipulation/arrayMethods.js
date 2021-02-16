2ac2549b32a502311cddc7bae881360407cf4682
tati oliveira <tat1roliv@gmail.com>
2021-02-16 20:03:59 -0300

    Update arrayMethods.js

--- Javascript/m3-manipulation/arrayMethods.js
+++ Javascript/m3-manipulation/arrayMethods.js
@@ -1,3 +1,4 @@
+///////////////////////////////////////////////////////////////////////////////1
 //to string
 let lista = [ 'ovo', 'farinha', 'acucar', 'leite' ];
 let res = lista.toString();
@@ -36,6 +37,28 @@ tea[0] = 'camomila';
 tea[3] = 'cha preto';//eh possivel alterar, mas é melhor usar o push
 let resTea = tea;
 console.log(resTea);
+
+///////////////////////////////////////////////////////////////////////////////2
+//splice -> remove um item do array
+//splice(posicao do item (a partir do item), quantidade de itens a remover - a partir daquele item marcado)
+let componentes = [ 'hd','placa video', 'processador', 'placa de rede' , 'gabinete'];
+componentes.splice(3,1);
+let resComp = componentes;
+console.log(resComp);
+
+//concat -> concatenar array
+let listaFeijao = [ 'feijao', 'verduras', 'calabresa','bacon' ];
+let listaArroz = [ 'arroz', 'alho','cenoura'];
+let resConcat = lista.concat(listaArroz);
+console.log(resConcat);
+
+//sort -> ordenamento (alfabetico)
+//reverse -> ordenamento reverso(alfabetico)
+let listaOrdemAlfa = [ 'Dnome','Anome', 'Bnome', 'Fnome', 'Enome', 'Cnome'];
+listaOrdemAlfa.sort();
+listaOrdemAlfa.reverse();//sen usar o sort antes -> vai inverter como estiver
+let resAlfa = listaOrdemAlfa;
+console.log(resAlfa);
 
 
 
