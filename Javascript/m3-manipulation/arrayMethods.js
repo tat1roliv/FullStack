e6198047844141df1b1ed277270551ee8f977c5f
tati oliveira <tat1roliv@gmail.com>
2021-02-16 21:25:08 -0300

    Update arrayMethods.js

--- Javascript/m3-manipulation/arrayMethods.js
+++ Javascript/m3-manipulation/arrayMethods.js
@@ -60,5 +60,90 @@ listaOrdemAlfa.reverse();//sen usar o sort antes -> vai inverter como estiver
 let resAlfa = listaOrdemAlfa;
 console.log(resAlfa);
 
+///////////////////////////////////////////////////////////////////////////////3
+//funcoes no array
+
+//map ->  aplicar funcao em cada item do array
+let numeros = [ 45, 4, 9, 16, 25 ];
+let numerosEdit = [];
+
+numerosEdit = numeros.map(function(item){  
+    return item * 2;
+});
+/*
+for (let i in numeros){
+    numerosEdit.push(lista[i] * 2);
+}
+*/
+let resNum = numerosEdit;
+console.log(resNum);
+
+//filter 
+// rodar uma função que retorna true (inclui no retorno) ou false (nao inclui no retorno)
+let numbers = [ 1, 2, 3, 4, 5 ];
+let numbersEdit = [];
+
+numbersEdit = numbers.filter(function(elemento){
+    if (elemento > 3){
+        return true;
+    } else{
+        return false;
+    }
+
+});
+
+let resN = numbersEdit;
+console.log(resN);
+
+//every -> verifica e retorna true or false (apenas), tudo ou nada
+//some -> pelo menos 1, tb retorna true ou false (apenas)
+let numerales = [ 11 , 22 , 33 , 44, 99 ];
+let numeralesEdit = [];
+
+numeralesEdit = numerales.every(function(dado){//numerales.some
+    if(dado > 20){ // ternario -> return (dado > 20)? true : false;
+        return true;
+    }else{
+        return false;
+    }
+});
+let resNumerales = numbersEdit;
+console.log(resNumerales);
+
+///////////////////////////////////////////////////////////////////////////////4
+
+//find -> procura se existe no array
+//findIndex -> posicao daquele item no array
+let namesss = [ 'paulo', 'maria', 'jose', 'ana' ];
+let nameEdit = [];
+
+nameEdit = namesss.find(function(artikel){//findIndex
+    if (artikel == 'ana'){
+        return true;
+    } else{
+        return false;
+    }
+});
+
+let resName = nameEdit;
+console.log(resName);
+
+
+//
+
+let turma = [
+    {id:1, nome: 'boni', sobrenome: 'lacerda'},
+    {id:2, nome: 'maria', sobrenome: 'pereira'},
+    {id:3, nome: 'pedro', sobrenome: 'silva'}
+];
+
+let pessoaProcurada = turma.find(function(calopsita){
+    return ( calopsita.sobrenome == 'silva' ) ? true : false;
+})
+
+let resTurma = pessoaProcurada;
+console.log(resTurma);
+
+
 
 
