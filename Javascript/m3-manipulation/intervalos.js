b1acbdd1497f778963795261e27b9387aec7371d
tati oliveira <tat1roliv@gmail.com>
2021-02-18 18:10:11 -0300

    intervalos(Time)

--- Javascript/m3-manipulation/intervalos.js
+++ Javascript/m3-manipulation/intervalos.js
@@ -1,3 +1,5 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+//setInterval -> roda a funcao de x em x tempo (nao para)
 //relogio
 function showTime(){
     let d = new Date();
@@ -10,20 +12,27 @@ function showTime(){
     document.querySelector(".demo").innerHTML = txt;
 
 }
-
 //let timer = setInterval(showTime, 1000);//mls
-
-
 let timer;
 
-//exibe tempo
 function startNow(){
-    let timer = setInterval(showTime, 1000);//mls
+    timer = setInterval(showTime, 1000);//mls
 }
 
-//para tempo
 function stopNow(){
     clearInterval(timer);
 }
+///////////////////////////////////////////////////////////
+//setTimeOut -> espera o tempo (parametro) e roda a funcao (uma unica vez)
+let timer2;
+function nowStart(){
+    timer2 = setTimeout(function(){
+        document.querySelector(".demo2").innerHTML = "rodando ok";
+    }, 1000)
+}
+function nonStop(){
+clearTimeout(timer2);
+}
 
-//cronometro
+
+
