51942066e089bba7880922cf9db864d1340bc6c6
tati oliveira <tat1roliv@gmail.com>
2021-02-23 11:24:43 -0300

    IncludeRepeat

---
+++ Javascript/m3-manipulation/IncludeRepeat.js
@@ -0,0 +1,14 @@
+//include
+
+let pf = [ 'ovo', 'arroz', 'feijão', 'macarrão', 'farofa'];
+
+console.log( lista.includes('ovo') );//return true
+console.log( lista.includes('carne') );//return false
+
+let nome = 'boni';
+console.log( ( nome.includes('a')) );//return false
+
+//repeat
+
+console.log( 'x'.repeat(20) );
+console.log( nome.repeat(10) );
