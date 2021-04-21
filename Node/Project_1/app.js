9acb7feb4939f244c47c91e48069805144d2dedf
tati oliveira <tat1roliv@gmail.com>
2021-04-21 18:55:03 -0300

    :up: routes

--- Node/Project_1/app.js
+++ Node/Project_1/app.js
@@ -6,4 +6,6 @@ const router = require('./routes/index');/*importing routes*/
 const app = express(); /*lib for server*/
 app.use('/', router);
 
+app.use(express.json()); /*req POST = req GET*/
+
 module.exports = app;
--- Node/Project_1/routes/index.js
+++ Node/Project_1/routes/index.js
@@ -1,10 +1,35 @@
 const express = require('express'); /*importing express lib*/
 
 //Routes
+
+/*req
+GET -> req.query.name
+POST -> update app.js
+*/
+
+/*req, res*/
 const router = express.Router();
 router.get('/', (req, res) => {
-    res.send('holaquetal reload remix');
+    res.json(req.query);
+
+    //GET: req.query
+    //POST: req.body
+    //URL PARAMS: req.params
+
+    //SEND
+    //JSON
+
 });
+
+
+router.get('/posts/:id', (req, res) => {
+    let slug = req.params.slug;
+    //titulo: hola que tal
+    //slug: hola-que-tal
+    res.send('SLUG do POST: '+ slug);
+});
+
+
 router.get('/about', (req, res) => {
     res.send('about me page');
 });
