4ae7ebda6d338ffcee8b03034cade3e34f3cee53
tati oliveira <tat1roliv@gmail.com>
2021-04-25 13:17:52 -0300

    creating json

---
+++ Javascript/m4-requisicoes/main.js
@@ -0,0 +1,10 @@
+let pessoa = {
+    nome : 'boni', 
+    sobrenome: 'maria',
+    idade: 99,
+    perfil: ["top", "topp", "toppp"],
+    estetica: {
+        altura: 2,
+        peso: 100
+    }
+}
