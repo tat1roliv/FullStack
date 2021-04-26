87dada3841bfc5dce61bb461241baef79facb805
tati oliveira <tat1roliv@gmail.com>
2021-04-26 11:26:11 -0300

    Create sync_async.js

---
+++ Javascript/m4-requisicoes/sync_async.js
@@ -0,0 +1,13 @@
+//SYNC
+let nome = 'boni';
+let sobrenome = 'lacerda';
+let completo = nome + ' ' + sobrenome;
+
+//ASYNC
+let nome = 'boni';
+let sobrenome = 'lacerda';
+let temp = maquina.getTemp(); //async
+let completo = nome + ' ' + sobrenome;
+
+//REQ INTERNA -> ASYNC 
+
