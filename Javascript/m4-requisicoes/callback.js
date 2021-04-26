3cbf506fd13a15347a0d85491c27ef18935f3a0a
tati oliveira <tat1roliv@gmail.com>
2021-04-26 11:34:24 -0300

    Create callback.js

---
+++ Javascript/m4-requisicoes/callback.js
@@ -0,0 +1,10 @@
+//callback
+
+function hola(){
+    console.log('holaquetal');
+}
+
+let nome = 'boni;';
+setTimeout(hola, 2000);
+let sobrenome = 'xi';
+console.log('nome completo' = nome + sobrenome)
