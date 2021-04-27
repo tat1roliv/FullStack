7344c07b6b9e1f68443b15e768983916f3ff6405
tati oliveira <tat1roliv@gmail.com>
2021-04-27 11:01:56 -0300

    Update fetch.js

--- Javascript/m4-requisicoes/fetch.js
+++ Javascript/m4-requisicoes/fetch.js
@@ -3,12 +3,13 @@ function loadPosts(){
     document.getElementById("posts").innerHTML =  'Loading...'
 
     fetch("https://jsonplaceholder.typicode.com/posts") //faz a requisicao
-        //get (default) - post - put - delete
+        //get (default) - post - put - patch - delete
         .then(function(resultado){
             return resultado.json(); //return a promise (espera o resultado e transforma em json)
         })
         .then(function(json){
-            document.getElementById("posts").innerHTML = json.length + ' posts'//retorna o resultado (json /api)
+            montarBlog(json);
+            //document.getElementById("posts").innerHTML = json.length + ' posts'//retorna o resultado (json /api)
         })
         //return a promise
         .catch(function(error){
@@ -16,3 +17,16 @@ function loadPosts(){
         })
 }
 /*erro de cors (pode acontecer se nao usar o protocolo http/server)*/
+
+function montarBlog(lista){
+    let html = '';
+
+    for (let i in lista){
+
+        html += '<h3>' + lista[i].title + '</h3>' ;
+        html += lista[i].body + '</br>';
+        html += '<hr/>'
+
+    }
+    document.getElementById("posts").innerHTML = html;
+}
