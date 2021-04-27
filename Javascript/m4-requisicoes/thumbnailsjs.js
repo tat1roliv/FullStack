b22f8168e0fbd029bb0d60cb4be2e7b386a34f49
tati oliveira <tat1roliv@gmail.com>
2021-04-27 12:23:27 -0300

    FileReader js

--- Javascript/m4-requisicoes/thumbnailsjs.js
+++ Javascript/m4-requisicoes/thumbnailsjs.js
@@ -10,3 +10,22 @@ function exibir(){
     document.getElementById("area").appendChild(img);
 
 }
+
+/*metodo filereader*/
+
+function mostrar(){
+    
+    let reader = new FileReader();
+    let imagem = document.getElementById('imagem').files[0];
+
+    reader.onloadend = function(){
+        let img = document.createElement('img');
+        img.src = reader.result;
+        img.width = 300;
+
+        document.getElementById('area').appendChild(img);
+    }
+
+    reader.readAsDataURL(imagem);
+
+}
