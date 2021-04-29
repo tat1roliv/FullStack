4f4adb330d6fa49bdfde0f9871a4a0ff194a657b
tati oliveira <tat1roliv@gmail.com>
2021-04-29 18:38:54 -0300

    angular project 1

--- Angular/v9/angularaulas/src/app/app.component.ts
+++ Angular/v9/angularaulas/src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  /*styleUrls: ['./app.component.css']*/
 })
 export class AppComponent {
   title = 'angularaulas';
+  nome = 'boni';
+  idade = 99;
+  nomes = [ 'ana', 'maria', 'holaa'];
+  nomePrincipal = this.nomes[0];
 }
