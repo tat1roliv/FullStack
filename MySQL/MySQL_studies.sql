9ef32bff694525cfce103ef7a50d64723a48f6a6
tati oliveira <tat1roliv@gmail.com>
2021-05-21 19:16:29 -0300

    mysql

---
+++ MySQL/MySQL_studies.sql
@@ -0,0 +1,10 @@
+use cadastro;
+create table pessoas(            
+nome varchar(30),
+idade tinyint(3),
+sexo char(1),
+peso float,
+altura float,
+nacionalidade varchar(20)
+);
+describe pessoas;
--- reactcontador/src/App.js
+++ reactcontador/src/App.js
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 function App() {
 
   const [ contagem, setContagem ] = useState(0);
+
+  //useeffect param(funcao anom, array);
+  //useEffect(() => {}, []);
+
+  useEffect(() => {
+    document.title = "Contagem: "+contagem;
+  }, [contagem]);//se nao for passado parametro, executa apenas uma vez
 
   const botaoAction = () => {
     setContagem(contagem + 1);
