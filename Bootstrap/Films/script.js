96bde1972862e7a7d57e76d4c7cb8b5709597882
tati oliveira <tat1roliv@gmail.com>
2021-07-25 12:48:20 -0300

    :up: api url

--- Bootstrap/Films/script.js
+++ Bootstrap/Films/script.js
@@ -1,7 +1,8 @@
 $(function(){
 
     $.ajax({
-        url:'https://alunos.b7web.com.br/cinema/',
+        /*url:'https://alunos.b7web.com.br/cinema/',*/
+        url: 'https://api.b7web.com.br/cinema/',
         type: 'GET',
         dataType: 'json',
         success: function(json){
