30a4307d70b4334d869cf38e94223ac8c9aad234
tati oliveira <tat1roliv@gmail.com>
2021-07-25 13:00:01 -0300

    js reading api data

--- Bootstrap/Films/script.js
+++ Bootstrap/Films/script.js
@@ -5,8 +5,16 @@ $(function(){
         url: 'https://api.b7web.com.br/cinema/',
         type: 'GET',
         dataType: 'json',
+        beforeSend: function(){
+            $('.filmsContent').html('<div class="col-md-12">Loading...</div>');
+        },
         success: function(json){
-            console.log(json);
+            var html = '';
+            
+            for(var i in json){
+                html += '<div class="col-md-4"><img src="'+json[i].avatar+'" />'+ json[i].titulo+'</div>';
+            }
+            $('.filmsContent').html(html);
         }
     });
 });
